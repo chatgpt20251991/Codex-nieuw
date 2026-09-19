535101d4f26214e4aff4c86ea9cd2c8be03843ec
chatgpt20251991 <younkha91@gmail.com>
2026-09-19 14:57:31 +0200

    Use unified Sunburst logo in website header and footer

--- websites/marketing/components/brand.tsx
+++ websites/marketing/components/brand.tsx
@@ -1,7 +1,7 @@
 import {localeHref, type Locale} from "@/lib/i18n";
+
 export function Brand({locale = "nl"}: {locale?: Locale}) {
   return <a className="brand" href={localeHref(locale, "/")} aria-label="EUbatterypassport.nl homepage">
-    <img className="brand-prefix" src="/images/prefix-v7.png" width="605" height="242" alt="" aria-hidden="true" />
-    <img className="brand-wordmark" src="/images/wordmark-v7.svg" width="1046" height="112" alt="" aria-hidden="true" />
+    <img className="brand-logo" src="/images/logo-sunburst-v9.webp" width="1800" height="208" alt="" aria-hidden="true" />
   </a>;
 }
